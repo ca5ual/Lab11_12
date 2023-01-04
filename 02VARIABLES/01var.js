9a3586f65581327756b82022159e8c206610184b
ca5ual <deniszhak4@gmail.com>
2023-01-04 20:53:54 +0100

    lab11_12

---
+++ 02VARIABLES/01var.js
@@ -0,0 +1,20 @@
+var x = 4;
+var y = 6;
+var z = x+y;
+
+var name = "Pawel";
+var person = "Karolina Kowalska", carName = "Nissan", price = 20000;
+
+var personA = "Karolina Kowalska",
+carNameA = "Nissan",
+priceA = 20000;
+
+var carNameB;
+var personX = "Janina" + "Nowak";
+
+var isFamous = true;
+
+var xx = 2 + 3 + "5";
+
+console.log(x, y, z, name, person, personA, carName, carNameA, price, priceA, carNameB, 
+    personX, isFamous, xx);
---
+++ 03MUST/01arrows.js
@@ -0,0 +1,18 @@
+const getFavouriteNumber = () => 6;
+const addSeven = a => a+7;
+const divide = (a,b) => a/b;
+
+//bez uzywania arrays
+
+function getFavouriteNumberA () {
+    return 6;
+}
+
+function addSevenA (a) {
+    return a + 7;
+}
+
+function divideA (a,b) {
+    return a/b;
+}
+
