3640bd0662d6bc7fcc0b2c6953ad9ed00c7d770d
ca5ual <deniszhak4@gmail.com>
2023-01-05 23:06:03 +0100

    lab11_12

--- 05Lab/selector01.js
+++ 05Lab/selector01.js
@@ -7,7 +7,15 @@
 //});
 
 $(document).ready(function () {
-    $("button").click(function () {
+    $("#button_01").click(function () {
         $("p").hide();
     });
-});
+    
+    $("#button_02").click(function () {
+        $("#p_03").hide();
+    });
+    
+    $("#button_03").click(function () {
+        $(".cp_01").hide ();
+    });
+    });
